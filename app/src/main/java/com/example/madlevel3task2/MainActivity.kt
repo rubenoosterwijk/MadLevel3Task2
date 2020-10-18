f895f97d6761dd61d25d11c257d12208f7db28ae
Ruben <ruben.oosterwijk@hva.nl>
2020-10-18 23:55:40 +0200

    Finished MadLevel3Task2

--- app/src/main/java/com/example/madlevel3task2/MainActivity.kt
+++ app/src/main/java/com/example/madlevel3task2/MainActivity.kt
@@ -6,8 +6,13 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.navigation.NavController
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,8 +21,15 @@ class MainActivity : AppCompatActivity() {
 
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+                .setAction("Action", null).show()
         }
+
+        navController = findNavController(R.id.nav_host_fragment)
+        fab.setOnClickListener {
+            navController.navigate(R.id.action_portalsFragment_to_addPortalFragment)
+        }
+
+        fabToggler()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -33,6 +45,16 @@ class MainActivity : AppCompatActivity() {
         return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun fabToggler() {
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if (destination.id in arrayOf(R.id.addPortalFragment)) {
+                fab.hide()
+            } else {
+                fab.show()
+            }
         }
     }
 }
